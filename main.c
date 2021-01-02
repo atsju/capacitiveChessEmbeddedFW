7dbd7859f1070728616b339d61604512989456d6
Julien Staub <atsju2@yahoo.fr>
2021-01-02 18:55:23 +0100

    add clock init for USB

--- main.c
+++ main.c
@@ -208,6 +208,8 @@ static void SystemClockHSI_Config(void)
 {
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
+  RCC_PeriphCLKInitTypeDef PeriphClkInitStruct = {0};
+  static RCC_CRSInitTypeDef RCC_CRSInitStruct;
 
   /* -1- Select MSI as system clock source to allow modification of the PLL configuration */
   RCC_ClkInitStruct.ClockType       = RCC_CLOCKTYPE_SYSCLK;
@@ -271,6 +273,40 @@ static void SystemClockHSI_Config(void)
     /* Initialization Error */
     Error_Handler();
   }
+
+
+  /* Enable the HSI48 Oscilator */
+  RCC_OscInitStruct.OscillatorType      = RCC_OSCILLATORTYPE_HSI48;
+  RCC_OscInitStruct.HSI48State          = RCC_HSI48_ON;
+  RCC_OscInitStruct.PLL.PLLState        = RCC_PLL_OFF;
+  HAL_RCC_OscConfig(&RCC_OscInitStruct);
+
+  /* Select HSI84 output as USB clock source */
+  PeriphClkInitStruct.PeriphClockSelection = RCC_PERIPHCLK_USB;
+  PeriphClkInitStruct.UsbClockSelection = RCC_USBCLKSOURCE_HSI48;
+  HAL_RCCEx_PeriphCLKConfig(&PeriphClkInitStruct);
+
+
+  /*Configure the clock recovery system (CRS)**********************************/
+
+  /* Enable CRS Clock */
+  __HAL_RCC_CRS_CLK_ENABLE();
+
+  /* Default Synchro Signal division factor (not divided) */
+  RCC_CRSInitStruct.Prescaler = RCC_CRS_SYNC_DIV1;
+
+  /* Set the SYNCSRC[1:0] bits according to CRS_Source value */
+  RCC_CRSInitStruct.Source = RCC_CRS_SYNC_SOURCE_USB;
+
+  /* HSI48 is synchronized with USB SOF at 1KHz rate */
+  RCC_CRSInitStruct.ReloadValue =  __HAL_RCC_CRS_RELOADVALUE_CALCULATE(48000000, 1000);
+  RCC_CRSInitStruct.ErrorLimitValue = RCC_CRS_ERRORLIMIT_DEFAULT;
+
+  /* Set the TRIM[5:0] to the default value*/
+  RCC_CRSInitStruct.HSI48CalibrationValue = 0x20;
+
+  /* Start automatic synchronization */
+  HAL_RCCEx_CRSConfig (&RCC_CRSInitStruct);
 }
 
 
